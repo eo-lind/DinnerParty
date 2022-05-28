9c2aa8b4574c67423fe0f0b4bc017442478ed13d
Olivia Lind <eo.lind@gmail.com>
2022-05-28 17:13:21 -0500

    Guest class created

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// TODO: Step 5: Call the method
+
+
+// TODO: Step 4: Create a method to seat the guests at a table
+
+
+// TODO: Step 2: Create a method to return a list (guests)
+
+
+    // TODO: Step 3: Create a list of objects (new Guest)
+
+
+// Step 1: create a class (Guest)
+public class Guest
+{
+    public string Name { get; set; }
+    public string Occupation { get; set; }
+    public string Bio { get; set; }
+}
