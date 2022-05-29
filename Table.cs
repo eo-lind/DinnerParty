dc37ff686c6a5f0da74f500370d223afa0cb8397
Olivia Lind <eo.lind@gmail.com>
2022-05-28 19:15:13 -0500

    created table class with list constructor inside

---
+++ Table.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace DinnerParty
+{
+    public class Table
+    {
+        public List<Guest> tables { get; set; } = new List<Guest>();
+    }
+}
